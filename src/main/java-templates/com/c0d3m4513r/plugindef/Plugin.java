27b3d0f2d42a1c4845a49210a9f9d24be3ad4a30
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2022-09-23 15:52:00 +0200

    initial Commit
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

---
+++ src/main/java-templates/com/c0d3m4513r/plugindef/Plugin.java
@@ -0,0 +1,9 @@
+package com.c0d3m4513r.plugindef;
+
+public final class Plugin {
+    public static final String id = "${id}";
+    public static final String name = "${name}";
+    public static final String description = "${description}";
+    public static final String configClass = "${mainConfigPath}";
+    public static final String version = "${versionName}";
+}
